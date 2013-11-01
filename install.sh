220fd7be9e9ca226c1334665e711a1346c071e19
Theo <theo@voinetworks.ca>
2013-10-31 23:42:47 -0500

    sed 's/eionet/voinet/g' basically

--- install.sh
+++ install.sh
@@ -9,22 +9,22 @@ OTRS_GROUP="apache"
 OTRS_ROOT="/opt/otrs"
 
 #make theme directory
-/bin/mkdir -p $OTRS_ROOT/Kernel/Output/HTML/EIONET
+/bin/mkdir -p $OTRS_ROOT/Kernel/Output/HTML/VOINET
 
 #Fix permissions and make the proper symlinks
 /bin/chown -R $OTRS_USER:$OTRS_GROUP $SKIN_PATH
-/bin/chown -R $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Output/HTML/EIONET
-/bin/chmod -R 2775 $SKIN_PATH/skins/Agent/eionet
-/bin/chmod -R 2775 $SKIN_PATH/skins/Customer/eionet
+/bin/chown -R $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Output/HTML/VOINET
+/bin/chmod -R 2775 $SKIN_PATH/skins/Agent/voinet
+/bin/chmod -R 2775 $SKIN_PATH/skins/Customer/voinet
 
-/bin/ln -s $SKIN_PATH/EionetSkin.xml $OTRS_ROOT/Kernel/Config/Files/ || true
-/bin/ln -s $SKIN_PATH/skins/Agent/eionet $OTRS_ROOT/var/httpd/htdocs/skins/Agent || true
-/bin/ln -s $SKIN_PATH/skins/Customer/eionet $OTRS_ROOT/var/httpd/htdocs/skins/Customer || true
+/bin/ln -s $SKIN_PATH/VoinetSkin.xml $OTRS_ROOT/Kernel/Config/Files/ || true
+/bin/ln -s $SKIN_PATH/skins/Agent/voinet $OTRS_ROOT/var/httpd/htdocs/skins/Agent || true
+/bin/ln -s $SKIN_PATH/skins/Customer/voinet $OTRS_ROOT/var/httpd/htdocs/skins/Customer || true
 
-/bin/cp -r $SKIN_PATH/theme/EIONET $OTRS_ROOT/Kernel/Output/HTML
+/bin/cp -r $SKIN_PATH/theme/VOINET $OTRS_ROOT/Kernel/Output/HTML
 
 echo ""
 echo "==========================================================="
-echo "All set. Now you can use the eionet skin and theme in otrs"
+echo "All set. Now you can use the voinet skin and theme in otrs"
 echo "==========================================================="
 echo ""
