326398b339d06b09a85197c134afe815ad6f12e1
Theo <theo@voinetworks.ca>
2013-12-22 21:34:38 -0600

    various updates

--- install.sh
+++ install.sh
@@ -14,13 +14,16 @@ OTRS_ROOT="/opt/otrs"
 #Fix permissions and make the proper symlinks
 /bin/chown -R $OTRS_USER:$OTRS_GROUP $SKIN_PATH
 /bin/chown -R $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Output/HTML/VOINET
-/bin/chmod -R 2775 $SKIN_PATH/skins/Agent/voinet
-/bin/chmod -R 2775 $SKIN_PATH/skins/Customer/voinet
+/usr/bin/find $SKIN_PATH/skins/ -type f -print0 | xargs -0 /bin/chmod 0660
+/usr/bin/find $SKIN_PATH/skins/ -type d -print0 | xargs -0 /bin/chmod 2770
 
 /bin/ln -sf $SKIN_PATH/VoinetSkin.xml $OTRS_ROOT/Kernel/Config/Files/ || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Config/Files/VoinetSkin.xml
 /bin/ln -sf $SKIN_PATH/skins/Agent/voinet $OTRS_ROOT/var/httpd/htdocs/skins/Agent || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Agent/voinet
 /bin/ln -sf $SKIN_PATH/skins/Customer/voinet $OTRS_ROOT/var/httpd/htdocs/skins/Customer || true
-
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Customer/voinet
+ 
 /bin/cp -r $SKIN_PATH/theme/VOINET $OTRS_ROOT/Kernel/Output/HTML
 
 echo ""
